e43d561fa2a42e4c2514d0f0afb0b54d257c2669
Zack Bleach <zackb@brandwatch.com>
2015-03-24 18:05:17 -0700

    Updated unit test

--- src/test/scala/com/zackbleach/pingpong/NicknameServletSpec.scala
+++ src/test/scala/com/zackbleach/pingpong/NicknameServletSpec.scala
@@ -1,6 +1,7 @@
 package com.zackbleach.pingpong
 
 import org.scalatra.test.specs2._
+import com.zackbleach.pingpong.controllers._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class NicknameServletSpec extends ScalatraSpec { def is =
@@ -8,7 +9,7 @@ class NicknameServletSpec extends ScalatraSpec { def is =
     "should return status 200"                  ! root200^
                                                 end
 
-  addServlet(classOf[NicknameServlet], "/*")
+  addServlet(classOf[NicknameController], "/*")
 
   def root200 = get("/") {
     status must_== 200
