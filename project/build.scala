788b5a6e0eb32ddb0973715058759cc9932a449c
Zack Bleach <zackb@brandwatch.com>
2015-03-25 14:27:02 -0700

    Updated travis conf

--- project/build.scala
+++ project/build.scala
@@ -8,7 +8,7 @@ import ScalateKeys._
 object PingpongnicknamegeneratorBuild extends Build {
   val Organization = "com.zackbleach"
   val Name = "pingpong-nickname-generator"
-  val Version = "0.1.0-SNAPSHOT"
+  val Version = "1"
   val ScalaVersion = "2.11.1"
   val ScalatraVersion = "2.3.0"
 
