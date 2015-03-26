e4bf452e1fb14a0dc689fee2a945bf67bf7c7a40
Zack Bleach <zackb@brandwatch.com>
2015-03-25 19:02:10 -0700

    updated test

--- src/test/scala/com/zackbleach/pingpong/NicknameServletSpec.scala
+++ src/test/scala/com/zackbleach/pingpong/NicknameServletSpec.scala
@@ -4,10 +4,12 @@ import org.scalatra.test.specs2._
 import com.zackbleach.pingpong.controllers._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class NicknameServletSpec extends ScalatraSpec { def is =
-  "GET / on NicknameServlet"                     ^
-    "should return status 200"                  ! root200^
+class NicknameServletSpec extends ScalatraSpec { def is = """
+  GET / on NicknameServlet
+    should return status 200                    $root200
+    should return a non-null nickname                     
                                                 end
+                                                """
 
   addServlet(classOf[NicknameController], "/*")
 
